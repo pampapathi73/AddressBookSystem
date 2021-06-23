a674514aa3e5abbdb05551e2c232905914031518
pampapathi73 <pampapathik32@gmail.com>
2021-06-23 12:28:58 +0530

    [pampapathi] Added: DeleteContactUsingName

--- AddressBookSystem/AddressBookBuider.cs
+++ AddressBookSystem/AddressBookBuider.cs
@@ -7,6 +7,7 @@ namespace AddressBookSystem
     class AddressBookBuider : IContacts
 {
         private LinkedList<Contacts> list = new LinkedList<Contacts>();
+        private Dictionary<string, AddressBookBuider> dictionary = new Dictionary<string, AddressBookBuider>();
         public void AddContact(string firstName, string lastName, string address, string city, string state, string email, int zip, long phoneNumber)
         {
             Contacts contact = new Contacts();
@@ -91,6 +92,39 @@ namespace AddressBookSystem
             }
             if (flag == false)
                 Console.WriteLine("not exits");
+        }
+        public void DeleteContact(string deletename)
+        {
+
+
+            if (list.Count > 0)
+            {
+                foreach (var contact in list)
+                {
+                    if (contact.FirstName == deletename)
+                    {
+                        list.Remove(contact);
+                        Console.WriteLine("deleted successfully");
+                        break;
+                    }
+
+                }
+                Console.WriteLine("not exists");
+            }
+            else
+                Console.WriteLine("list is empty,cannot delete");
+        }
+
+
+        public void AddAddressBook(string bookName)
+        {
+            AddressBookBuider addressBook = new AddressBookBuider();
+            dictionary.Add(bookName, addressBook);
+            Console.WriteLine("AddressBook Created.");
+        }
+        public Dictionary<string, AddressBookBuider> GetAddressBook()
+        {
+            return dictionary;
         }
 
     }
